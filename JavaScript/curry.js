51cfa3cd86e9d754d494ea44d7d63332d2f43de0
wudaxue <1203588983@qq.com>
2023-12-09 20:29:23 +0800

    常规属性+排序属性

--- JavaScript/curry.js
+++ JavaScript/curry.js
@@ -2,7 +2,7 @@
 // 所谓柯里化， 就是把一个多参数的函数转化为单一的参数函数，curry有3个作用： 缓存函数、暂缓函数、分解执行任务
 
 function sum(x) {
-  return function(y) {
+  return function (y) {
     return x + y
   }
 }
@@ -10,9 +10,9 @@ function sum(x) {
 
 
 // eg:
-sum(1).valueOf() => 1
-sum(1, 2).valueOf() => 2
-sum(1, 2)(3).valueOf => 6
+// sum(1).valueOf() => 1
+// sum(1, 2).valueOf() => 2
+// sum(1, 2)(3).valueOf => 6
 
 function sum(...args) {
   let saveArg = args || []
@@ -20,7 +20,7 @@ function sum(...args) {
     saveArg = saveArg.concat(args)
     return sub
   }
-  sub.valueOf = function() {
+  sub.valueOf = function () {
     return saveArg.reduce((a, b) => a + b, 0)
   }
   return sub
